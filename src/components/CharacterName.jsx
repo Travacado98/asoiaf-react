876ab5e21795f0cbffeda7d44e34be1c6446f89d
Travacado98 <travissmith.ts98@gmail.com>
2022-05-06 12:50:06 -0700

    character pagination

--- src/components/CharacterName.jsx
+++ src/components/CharacterName.jsx
@@ -7,6 +7,6 @@ export default function CharacterName({ charId }) {
   if (!character) return null;
 
   return (
-    <h1>{ character.name }</h1>
+    <h1>{character.name}</h1>
   );
 }
--- src/pages/AllCharacterList.jsx
+++ src/pages/AllCharacterList.jsx
@@ -1,33 +1,40 @@
 // import styles from "./App.module.css";
-
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import CharacterName from '../components/CharacterName';
-import { BOOK_IMAGES } from '../constants';
 
 export default function AllCharacterList() {
-  const [characters, setCharacters] = useState();
+  useEffect(() => {
+    document.title = 'ASOIAF | Characters';
+  }, []);
 
-  const fetchCharacters = () => {
+  const [characters, setCharacters] = useState();
+  const [page, setPage] = useState(1);
+  const [lastPage, setLastPage] = useState();
+
+  const getLastPage = (response) => {
+    const linkHeader = [...response.headers].find((header) => header[0] === 'link')[1];
+
+    const lastUrl = linkHeader.split(',').find((item) => item.includes('last')).trim().split(';')[0].replace('<', '').replace('>', '');
+
+    const urlSearchParams = new URLSearchParams(lastUrl.split('?')[1]);
+    const params = Object.fromEntries(urlSearchParams.entries());
+
+    return parseInt(params.page, 10);
+  };
+
+  const fetchCharacters = async () => {
     setCharacters(null);
 
-    fetch('https://anapioficeandfire.com/api/books?pageSize=50')
-      .then((response) => response.json())
-      .then((data) => {
-        const filteredBooks = data.filter((book) => BOOK_IMAGES[book.name]);
-        let combinedCharacters = [];
-        filteredBooks.forEach((book) => {
-          combinedCharacters = [...combinedCharacters, ...book.povCharacters];
-        });
-        combinedCharacters = [...new Set(combinedCharacters)];
-        setCharacters(combinedCharacters);
-      });
+    const response = await fetch(`https://anapioficeandfire.com/api/characters?pageSize=50&page=${page}`);
+    if (!lastPage) setLastPage(getLastPage(response));
+    const data = await response.json();
+    setCharacters(data);
   };
 
   useEffect(() => {
     fetchCharacters();
-    document.title = 'ASOIAF | Characters';
-  }, []);
+  }, [page]);
 
   if (!characters) return null;
 
@@ -36,10 +43,10 @@ export default function AllCharacterList() {
       <h2>Main Series</h2>
       <div className="row">
         {characters.map((character) => {
-          const charId = character.split('/').slice(-1)[0];
+          const charId = character.url.split('/').slice(-1)[0];
           return (
             <div className="col-4" key={charId}>
-              <Link to={`/character/${charId}`}>
+              <Link to={`/characters/${charId}`}>
                 <div className="card mb-4">
                   <CharacterName charId={charId} />
                 </div>
@@ -48,6 +55,12 @@ export default function AllCharacterList() {
           );
         })}
       </div>
+      {page > 1 && (
+        <button type="button" onClick={() => setPage(page - 1)}>Previous</button>
+      )}
+      {page < lastPage && (
+        <button type="button" onClick={() => setPage(page + 1)}>Next</button>
+      )}
     </>
   );
 }
